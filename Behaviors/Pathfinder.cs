37c55b15a1a0987600dca06adc53a368935e80f4
Tommy Hoffmann <tommy [DOT] hoffmann2 [AT] gmail [DOT] com>
2014-01-25 14:14:17 -0700

    Added pathfinder

---
+++ Behaviors/Pathfinder.cs
@@ -0,0 +1,16 @@
+﻿namespace MinionsOfDeath.Behaviors
+{
+    internal class Pathfinder : Action
+    {
+        public Pathfinder(Minion owner, Minion target)
+        {
+            _owner = owner;
+            _target = target;
+        }
+
+        public override DoublePoint GetGoal()
+        {
+            return _target.Pos;
+        }
+    }
+}
