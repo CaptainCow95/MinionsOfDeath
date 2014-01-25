211b53846d0f2dfc31507d0fa432990431057e61
Jay Gloster <jgloster@du.edu>
2014-01-24 23:14:27 -0700

    Adding behaviors

---
+++ Behaviors/Move.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinionsOfDeath.Behaviors
+{
+    class Move
+    {
+        private float _dV, _dTheta;
+
+        public float DV
+        {
+            set { _dV = value; }
+            get { return _dV;  }
+        }
+
+        public float _DTheta
+        {
+            set { _dTheta = value;  }
+            get { return _dTheta; }
+        }
+
+    }
+}
