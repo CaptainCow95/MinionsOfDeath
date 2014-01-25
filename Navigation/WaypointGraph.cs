95e6f87d6814b7decfb0fa24dd942851cc16bf77
Tommy Hoffmann <tommy [DOT] hoffmann2 [AT] gmail [DOT] com>
2014-01-24 22:51:31 -0700

    added getDistance

--- Navigation/WaypointGraph.cs
+++ Navigation/WaypointGraph.cs
@@ -1,6 +1,18 @@
-﻿namespace MinionsOfDeath.Navigation
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinionsOfDeath.Navigation
 {
-    internal class WaypointGraph
+    class WaypointGraph
     {
+        private List<WaypointNode> nodes;
+
+        private double getDistance(WaypointNode node1, WaypointNode node2)
+        {
+            return Math.Sqrt(Math.Pow(node1.X - node2.X, 2) + Math.Pow(node1.Y - node1.Y, 2));
+        }
     }
 }
--- Navigation/WaypointNode.cs
+++ Navigation/WaypointNode.cs
@@ -1,6 +1,32 @@
-﻿namespace MinionsOfDeath.Navigation
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinionsOfDeath.Navigation
 {
-    internal class WaypointNode
+    class WaypointNode
     {
+        private List<WaypointNode> neighbors;
+        private int _x;
+        private int _y;
+
+        public WaypointNode(int x, int y)
+        {
+
+        }
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
     }
 }
