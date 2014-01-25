a2e3bb25fd5a5dc3070e50a8230e7d3b5684fa31
Lewis Fox <LRFLEW@aol.com>
2014-01-25 12:32:10 -0700

    Fixed Scrolling w/ mouse off of sprite

--- Interface/ScrollBar.cs
+++ Interface/ScrollBar.cs
@@ -9,6 +9,7 @@ namespace MinionsOfDeath.Interface
         private bool _horizontal;
         private int _maxValue;
         private int _minValue;
+		private bool _scrolling = false;
         private Sprite _scrollBar;
 
         public ScrollBar(int x, int y, int width, int height, int minValue, int maxValue, bool horizontal, Sprite scrollBar)
@@ -53,9 +54,15 @@ namespace MinionsOfDeath.Interface
 
         public override void Update(double timeSinceFrame)
         {
-            if (Game.MouseState.LeftButton == OpenTK.Input.ButtonState.Pressed && Game.PreviousMouseState.LeftButton == OpenTK.Input.ButtonState.Pressed &&
-               Game.PreviousMousePosition.X > _scrollBar.X - Camera.X && Game.PreviousMousePosition.X < _scrollBar.X + _scrollBar.Width - Camera.Y &&
-               Game.PreviousMousePosition.Y > _scrollBar.Y - Camera.X && Game.PreviousMousePosition.Y < _scrollBar.Y + _scrollBar.Height - Camera.Y)
+			if (Game.MouseState.LeftButton == OpenTK.Input.ButtonState.Pressed &&
+			    Game.PreviousMousePosition.X > _scrollBar.X - Camera.X && Game.PreviousMousePosition.X < _scrollBar.X + _scrollBar.Width - Camera.Y &&
+			    Game.PreviousMousePosition.Y > _scrollBar.Y - Camera.X && Game.PreviousMousePosition.Y < _scrollBar.Y + _scrollBar.Height - Camera.Y) {
+				_scrolling = true;
+			} else if (Game.MouseState.LeftButton == OpenTK.Input.ButtonState.Released)
+			{
+				_scrolling = false;
+			}
+			if (_scrolling)
             {
                 if (_horizontal)
                 {
