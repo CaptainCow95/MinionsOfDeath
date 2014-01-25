2f6bedbc9f860b6af3bcfc1864cdd54b2e2442ec
Tommy Hoffmann <tommy [DOT] hoffmann2 [AT] gmail [DOT] com>
2014-01-25 16:56:23 -0700

    IsEnemyClose Query

--- Behaviors/Queries/IsEnemyClose.cs
+++ Behaviors/Queries/IsEnemyClose.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MinionsOfDeath.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,23 @@ namespace MinionsOfDeath.Behaviors.Queries
     internal class IsEnemyClose : QueryNode
     {
 
+        double closeDist = 200;
+
         public override DoublePoint GetGoal()
         {
-            if(true)
+            bool player1 = Game.Player1.Minions.ContainsValue(_owner);
+            bool succeed;
+            if (player1)
+            {
+                Minion minion = Game.Player2.Minions.Values.OrderBy(e => WaypointGraph.getDistance((int)_owner.Pos.X, (int)e.Pos.X, (int)_owner.Pos.Y, (int)e.Pos.Y)).First();
+                succeed = WaypointGraph.getDistance((int)_owner.Pos.X, (int)minion.Pos.X, (int)_owner.Pos.Y, (int)minion.Pos.Y) < closeDist;
+            }
+            else
+            {
+                Minion minion = Game.Player1.Minions.Values.OrderBy(e => WaypointGraph.getDistance((int)_owner.Pos.X, (int)e.Pos.X, (int)_owner.Pos.Y, (int)e.Pos.Y)).First();
+                succeed = WaypointGraph.getDistance((int)_owner.Pos.X, (int)minion.Pos.X, (int)_owner.Pos.Y, (int)minion.Pos.Y) < closeDist;
+            }
+            if(succeed)
             {
                 return TrueChild.GetGoal();
             }
