332003a291e71d081587e02f2fce0a9d58d46f7e
Nathan Clarke <CaptainCow95@gmail.com>
2014-01-25 10:20:15 -0700

    Minions now collide

--- GameObject.cs
+++ GameObject.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 
 namespace MinionsOfDeath
 {
-	internal abstract class GameObject
+    internal abstract class GameObject
     {
-		private List<Sprite> _sprites;
-		private int _state;
-		protected DoublePoint _pos = new DoublePoint(0, 0);
+        protected DoublePoint _pos = new DoublePoint(0, 0);
+        private double _height;
+        private List<Sprite> _sprites;
+        private int _state;
+        private double _width;
+
+        public GameObject(List<Sprite> sprites)
+        {
+            _sprites = sprites;
+
+            if (_sprites.Count > 0)
+            {
+                _width = _sprites[0].Width;
+                _height = _sprites[0].Height;
+            }
+        }
+
+        public double Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
 
         public DoublePoint Pos
         {
@@ -15,32 +34,35 @@ namespace MinionsOfDeath
             get { return _pos; }
         }
 
-		public GameObject(List<Sprite> sprites)
-		{
-			_sprites = sprites;
-		}
-
-		public int State
-		{
-			get { return _state; }
-			set { _state = value; }
-		}
-
-		public Sprite GetSprite()
-		{
-			return _sprites[_state];
-		}
-
-        public virtual void Update(double time)
+        public int State
         {
-            _sprites[_state].Update(time);
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public double Width
+        {
+            get { return _width; }
+            set { _width = value; }
         }
 
         public void Draw()
         {
             _sprites[_state].X = _pos.X;
             _sprites[_state].Y = _pos.Y;
+            _sprites[_state].Width = _width;
+            _sprites[_state].Height = _height;
             _sprites[_state].Draw();
+        }
+
+        public Sprite GetSprite()
+        {
+            return _sprites[_state];
+        }
+
+        public virtual void Update(double time)
+        {
+            _sprites[_state].Update(time);
         }
     }
 }
--- Minion.cs
+++ Minion.cs
@@ -6,37 +6,19 @@ namespace MinionsOfDeath
 {
     internal class Minion : GameObject
     {
-		private static double speed = 1.0;
-        
+        private static double speed = 1.0;
+
         //dictates the movement behavior of the minion
-        DecisionTree _decisionTree;
-        
-        //identifies each minion on a team
-        private int _MinionID;
+        private DecisionTree _decisionTree;
 
         //determines if the minion is special, and therfore if it will end the game when reaching the enemy base
         private bool _isSpecial;
-        
+
+        //identifies each minion on a team
+        private int _MinionID;
+
         //Represent the player the Minion belongs to
         private int _pid;
-
-        public int PID
-        {
-            get { return _pid; }
-            set { _pid = value; }
-        } 
-
-        public bool IsSpecial
-        {
-            get { return _isSpecial; }
-            set { _isSpecial = value; }
-        }
-
-        public int ID
-        {
-            set { _MinionID = value; }
-            get {return _MinionID;}
-        }
 
         public Minion(List<Sprite> sprites, int MinionID)
             : base(sprites)
@@ -45,21 +27,44 @@ namespace MinionsOfDeath
             _isSpecial = false;
         }
 
-
         public DecisionTree DecisionTree
         {
             get { return _decisionTree; }
             set { _decisionTree = value; }
-		}
+        }
 
-		public override void Update(double time)
-		{
+        public int ID
+        {
+            set { _MinionID = value; }
+            get { return _MinionID; }
+        }
+
+        public bool IsSpecial
+        {
+            get { return _isSpecial; }
+            set { _isSpecial = value; }
+        }
+
+        public int PID
+        {
+            get { return _pid; }
+            set { _pid = value; }
+        }
+
+        public bool IsCollidingWith(Minion minion)
+        {
+            return Pos.X < minion.Pos.X + minion.Width && Pos.X + Width > minion.Pos.X &&
+                Pos.Y < minion.Pos.Y + minion.Height && Pos.Y + Height > minion.Pos.Y;
+        }
+
+        public override void Update(double time)
+        {
             base.Update(time);
 
-			DoublePoint fp = _decisionTree.GetGoal();
-			DoublePoint v = new DoublePoint (fp.X - _pos.X, fp.Y - _pos.Y);
-			v.SetToLessOrEqualMag (speed);
-			_pos.Add (v);
-		}
+            DoublePoint fp = _decisionTree.GetGoal();
+            DoublePoint v = new DoublePoint(fp.X - _pos.X, fp.Y - _pos.Y);
+            v.SetToLessOrEqualMag(speed);
+            _pos.Add(v);
+        }
     }
 }
