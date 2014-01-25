46321a3903b1c31d739151c528c9eae9c524e156
Nathan Clarke <CaptainCow95@gmail.com>
2014-01-24 22:12:42 -0700

    Adding StringDrawer

---
+++ Graphics/StringDrawer.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK;
+using QuickFont;
+
+namespace MinionsOfDeath.Graphics
+{
+    public static class StringDrawer
+    {
+        public static void Draw(QFont font, string text, QFontAlignment alignment, Vector2 position)
+        {
+            font.Options.UseDefaultBlendFunction = false;
+            font.Print(text, alignment, position);
+        }
+    }
+}
