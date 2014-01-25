1a7773ce7de11700f50633f8811241b398710fdc
Nathan Clarke <CaptainCow95@gmail.com>
2014-01-25 08:58:57 -0700

    Added ScrollBar Update Logic

--- Interface/ScrollBar.cs
+++ Interface/ScrollBar.cs
@@ -23,14 +23,30 @@ namespace MinionsOfDeath.Interface
 		{
 			if (_horizontal) {
 				_scrollBar.X = ((double)_currentValue / (double)(_maxValue - _minValue)) * (Width - _scrollBar.Width);
-				_scrollBar.Y = Y + Height / 2;
+				_scrollBar.Y = Y;
+				_scrollBar.Height = Height;
 			} else {
+				_scrollBar.X = X;
+				_scrollBar.Y = ((double)_currentValue / (double)(_maxValue - _minValue)) * (Height - _scrollBar.Height);
+				_scrollBar.Width = Width;
 			}
+
 			_scrollBar.Draw ();
 		}
 
 		public override void Update (double timeSinceFrame)
 		{
+			if (Game.MouseState.LeftButton == OpenTK.Input.ButtonState.Pressed && Game.PreviousMouseState.LeftButton == OpenTK.Input.ButtonState.Pressed &&
+			   Game.PreviousMouseState.X > _scrollBar.X && Game.PreviousMouseState.X < _scrollBar.X + _scrollBar.Width &&
+			   Game.PreviousMouseState.Y > _scrollBar.Y && Game.PreviousMouseState.Y < _scrollBar.Y + _scrollBar.Height) {
+				if (_horizontal) {
+					_currentValue = (Game.MouseState.X - _minValue) / (_maxValue - _minValue);
+				} else {
+					_currentValue = (Game.MouseState.Y - _minValue) / (_maxValue - _minValue);
+				}
+
+				_currentValue = Math.Max (_minValue, Math.Min (_maxValue, _currentValue));
+			}
 		}
 	}
 }
