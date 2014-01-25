47ae0bade7edf3c7525b67451b7e4ec5e178d249
Lewis Fox <LRFLEW@aol.com>
2014-01-24 23:03:34 -0700

    Made GameObject a multi-sprite object
    Yay

--- GameObject.cs
+++ GameObject.cs
@@ -1,6 +1,26 @@
-﻿namespace MinionsOfDeath
+﻿using MinionsOfDeath.Graphics;
+
+namespace MinionsOfDeath
 {
-    internal class GameObject
+	internal class GameObject
     {
+		private Sprite[] _sprites;
+		private int _state;
+
+		public GameObject(Sprite[] sprites)
+		{
+			_sprites = sprites;
+		}
+
+		public int State
+		{
+			get { return _state; }
+			set { _state = value; }
+		}
+
+		public Sprite getSprite()
+		{
+			return _sprites[_state];
+		}
     }
 }
