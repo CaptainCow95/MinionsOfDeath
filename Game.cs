cebe0ddfb933c06c87b309e1edb2c0ded3a642d0
Nathan Clarke <CaptainCow95@gmail.com>
2014-01-24 21:37:59 -0700

    Added Sprite
    Added sprite drawing.

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK;
+﻿using MinionsOfDeath.Graphics;
+using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
 namespace MinionsOfDeath
@@ -9,18 +10,34 @@ namespace MinionsOfDeath
         {
             this.UpdateFrame += Game_UpdateFrame;
             this.RenderFrame += Game_RenderFrame;
+
+            _sprite = new Sprite("XRay-High.png");
         }
+
+        Sprite _sprite;
 
         private void Game_RenderFrame(object sender, FrameEventArgs e)
         {
-            GL.Viewport(this.ClientRectangle);
-            GL.Ortho(0, Width, Height, 0, 0, 1);
+            GL.Viewport(0, 0, Width, Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.PushMatrix();
+            GL.LoadIdentity();
+            GL.Ortho(0, Width, Height, 0, -1, 1);
+
+            GL.Disable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.Blend);
+            GL.Enable(EnableCap.Texture2D);
+
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
 
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
 
             GL.ClearColor(OpenTK.Graphics.Color4.CornflowerBlue);
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            _sprite.Draw();
 
             SwapBuffers();
         }
---
+++ Graphics/Sprite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace MinionsOfDeath.Graphics
+{
+    public class Sprite
+    {
+        public Sprite(string filename)
+        {
+            Bitmap bitmap = new Bitmap(filename);
+            _textureWidth = bitmap.Width;
+            _textureHeight = bitmap.Height;
+
+            _textureId = GL.GenTexture();
+
+            GL.BindTexture(TextureTarget.Texture2D, _textureId);
+
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            bitmap.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+        }
+        private int _textureId;
+        private int _textureWidth;
+        private int _textureHeight;
+        public void Draw()
+        {
+            GL.BindTexture(TextureTarget.Texture2D, _textureId);
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(0, 0);
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(0, _textureHeight);
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(_textureWidth, _textureHeight);
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(_textureWidth, 0);
+            GL.End();
+        }
+    }
+}
