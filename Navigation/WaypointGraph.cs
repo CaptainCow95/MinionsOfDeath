5af06a50cef246e65690629dcc87cdab9915fca2
Nathan Clarke <CaptainCow95@gmail.com>
2014-01-25 11:12:11 -0700

    References added

--- Navigation/WaypointGraph.cs
+++ Navigation/WaypointGraph.cs
@@ -1,40 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MinionsOfDeath.Navigation
 {
+    public class Connection
+    {
+        private double cost;
+        private WaypointNode fromNode;
+        private WaypointNode toNode;
+
+        public Connection(double cost, WaypointNode fromNode, WaypointNode toNode)
+        {
+            this.cost = cost;
+            this.fromNode = fromNode;
+            this.toNode = toNode;
+        }
+
+        public double getCost()
+        {
+            return cost;
+        }
+
+        public WaypointNode getFromNode()
+        {
+            return fromNode;
+        }
+
+        public WaypointNode getToNode()
+        {
+            return toNode;
+        }
+    }
+
+    public class NodeRecord
+    {
+        private Connection connection;
+        private double costSoFar;
+        private WaypointNode node;
+
+        public NodeRecord()
+        {
+        }
+
+        public NodeRecord(WaypointNode node)
+        {
+            this.node = node;
+            costSoFar = 0;
+        }
+
+        public Connection Connection
+        {
+            get { return connection; }
+            set { connection = value; }
+        }
+
+        public double CostSoFar
+        {
+            get { return costSoFar; }
+            set { costSoFar = value; }
+        }
+
+        public WaypointNode Node
+        {
+            get { return node; }
+            set { node = value; }
+        }
+    }
+
     public class WaypointGraph
     {
         // Storing nodes in dictionary, indexed by Tuple<_x,_y>
-        private Dictionary<Tuple<int,int>,WaypointNode> nodes;
-
+        private Dictionary<Tuple<int, int>, WaypointNode> nodes;
 
         public WaypointGraph(string filename)
         {
-            XmlTextReader reader = new XmlTextReader();
         }
+
         private void addNode(int x, int y)
         {
             //TODO: give node its neighbors
             List<WaypointNode> ns = new List<WaypointNode>();
-            WaypointNode n = new WaypointNode(this,x,y, ns);
-            nodes.Add(Tuple.Create(x,y),n);
-        }
-
-        private WaypointNode getNode(int x, int y)
-        {
-            return nodes[Tuple.Create(x, y)];
+            WaypointNode n = new WaypointNode(this, x, y, ns);
+            nodes.Add(Tuple.Create(x, y), n);
         }
 
         private double getDistance(WaypointNode node1, WaypointNode node2)
         {
             return getDistance(node1.X, node2.X, node1.Y, node2.Y);
         }
-        private double getDistance(int x1, int x2, int y1, int y2){
-            return Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1, 2));
+
+        private double getDistance(int x1, int x2, int y1, int y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        private WaypointNode getNode(int x, int y)
+        {
+            return nodes[Tuple.Create(x, y)];
         }
 
         private List<WaypointNode> pathfindDijkstra(WaypointNode start, WaypointNode end)
@@ -46,7 +109,8 @@ namespace MinionsOfDeath.Navigation
             List<NodeRecord> closed = new List<NodeRecord>();
             NodeRecord current = new NodeRecord();
 
-            while (open.Count > 0){
+            while (open.Count > 0)
+            {
                 current = open.Where(e => e.CostSoFar == open.Min(f => f.CostSoFar)).First();
 
                 if (current.Node == end)
@@ -57,19 +121,20 @@ namespace MinionsOfDeath.Navigation
                 foreach (WaypointNode n in current.Node.Neighbors)
                 {
                     WaypointNode fromNode = current.Node;
-                    Connection c = new Connection(getDistance(current.Node, n), current.Node , n);
+                    Connection c = new Connection(getDistance(current.Node, n), current.Node, n);
                     connections.Add(c);
                 }
 
-                foreach (Connection connection in connections){
+                foreach (Connection connection in connections)
+                {
                     WaypointNode endNode = connection.getToNode();
                     double endNodeCost = current.CostSoFar + connection.getCost();
                     NodeRecord endNodeRecord = closed.Where(e => e.Node == endNode).First();
-                    if (endNodeRecord==null)
+                    if (endNodeRecord == null)
                         endNodeRecord = open.Where(e => e.Node == endNode).First();
 
                     // if (closed.Contains(endNode))
-                    if(closed.Contains(endNodeRecord))
+                    if (closed.Contains(endNodeRecord))
                         continue;
                     // else if (open.Contains(endNode))
                     else if (open.Contains(endNodeRecord))
@@ -91,73 +156,26 @@ namespace MinionsOfDeath.Navigation
                     }
                 }
                 open.Remove(current);
-                closed.Add(current);    
+                closed.Add(current);
             }
-            if (current.Node != end){
+            if (current.Node != end)
+            {
                 return null;
             }
             else
             {
                 List<WaypointNode> path = new List<WaypointNode>();
                 path.Add(current.Connection.getFromNode());
-                while (current.Node != start){
+                while (current.Node != start)
+                {
                     // path += current.connection;
-                    path.Add(current.Connection.getToNode()); 
+                    path.Add(current.Connection.getToNode());
                     // current = current.connection.getFromNode();
                     current = closed.Where(e => e.Node == current.Connection.getFromNode()).First();
                 }
                 path.Reverse();
                 return path;
             }
-        }
-    }
-
-
-    public class Connection
-    {
-        double cost;
-        WaypointNode fromNode;
-        WaypointNode toNode;
-
-        public Connection(double cost, WaypointNode fromNode, WaypointNode toNode)
-        {
-            this.cost = cost;
-            this.fromNode = fromNode;
-            this.toNode = toNode;
-        }
-
-        public double getCost() { return cost; }
-        public WaypointNode getFromNode() { return fromNode; }     
-        public WaypointNode getToNode() { return toNode; }
-    }
-     
-    public class NodeRecord
-    {
-        WaypointNode node;
-        Connection connection;
-        double costSoFar;
-
-        public NodeRecord() { }
-        public NodeRecord(WaypointNode node)
-        {
-            this.node = node;
-            costSoFar = 0;
-        }
-
-        public double CostSoFar
-        {
-            get { return costSoFar; }
-            set { costSoFar = value; }
-        }
-        public WaypointNode Node
-        {
-            get { return node; }
-            set { node = value; }
-        }
-        public Connection Connection
-        {
-            get { return connection; }
-            set { connection = value; }
         }
     }
 }
