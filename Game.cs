ac7ad06077503c519f059d9793e5b94b17fe914d
Nathan Clarke <CaptainCow95@gmail.com>
2014-01-24 20:18:23 -0700

    Adding Base Game
    Adding the base game object and the main method.

---
+++ Game.cs
@@ -0,0 +1,26 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinionsOfDeath
+{
+    public class Game : GameWindow
+    {
+        public Game()
+        {
+            this.UpdateFrame += Game_UpdateFrame;
+            this.RenderFrame += Game_RenderFrame;
+        }
+
+        void Game_UpdateFrame(object sender, FrameEventArgs e)
+        {
+        }
+
+        void Game_RenderFrame(object sender, FrameEventArgs e)
+        {
+        }
+    }
+}
