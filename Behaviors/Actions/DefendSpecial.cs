0dbc11d165c4f05613517e98a9ff0e116a65b436
Tommy Hoffmann <tommy [DOT] hoffmann2 [AT] gmail [DOT] com>
2014-01-26 12:34:19 -0700

    added DefendSpecial action

---
+++ Behaviors/Actions/DefendSpecial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinionsOfDeath.Behaviors.Actions
+{
+    public class DefendSpecial : Action
+    {
+        public DefendSpecial(Minion owner)
+            : base(owner)
+        {
+        }
+
+        public override DoublePoint GetGoal()
+        {
+            return null;
+        }
+    }
+
+
+}
