3805875141160fb49fe0279bb6956bb5bfdc3ad8
Tommy Hoffmann <tommy [DOT] hoffmann2 [AT] gmail [DOT] com>
2014-01-25 22:35:17 -0700

    Base

---
+++ Base.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinionsOfDeath
+{
+    public class Base : GameObject
+    {
+        private int _pid;
+
+
+        public int PID
+        {
+            get { return _pid; }
+            set { _pid = value; }
+        }
+    }
+}
